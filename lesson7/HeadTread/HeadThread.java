9da8b7b8bc89ba2e05e80255868e1c1dc00ac072
Maxym Kalysh <503292@ukr.net>
2018-09-19 18:58:24 +0300

    HeadThread

---
+++ lesson7/HeadTread/HeadThread.java
@@ -0,0 +1,18 @@
+package lesson7.HeadTread;
+
+public class HeadThread {
+	public static void main(String args[]) {
+		Thread t = Thread.currentThread();
+		System.out.println(" Teкyщий поток исполнения : " + t);
+
+		t.setName("My Thread");
+		System.out.println(" Пocлe изменения имени потока : " + t);
+		try {
+			for (int n = 5; n > 0; n--)
+				System.out.println(n);
+			Thread.sleep(1000);
+		} catch (InterruptedException е) {
+			System.out.println("Глaвный поток исполнения прерван");
+		}
+	}
+}
