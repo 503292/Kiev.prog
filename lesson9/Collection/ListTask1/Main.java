db859c7d620565d44551f8e0e5afadd663f3ec18
Maxym Kalysh <503292@ukr.net>
2018-09-26 18:07:54 +0300

    ArrayList 10

---
+++ lesson9/Collection/ListTask1/Main.java
@@ -0,0 +1,33 @@
+package lesson9.Collection.ListTask1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Integer> myList = new ArrayList<>();
+		fillList(myList);
+		System.out.println(myList);
+
+		changeList(myList);
+		System.out.println(myList);
+		
+
+	}
+	
+	public static void fillList(List<Integer> myList) {
+		for(int i = 0; i < 10; i++) {
+			myList.add((int)(Math.random() * 100));
+			
+		}
+	}
+	public static void changeList(List<Integer> myList) {
+		myList.remove(0);
+		myList.remove(0);
+		myList.remove(myList.size() - 1);
+	}
+
+}
