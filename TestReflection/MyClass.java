0c095cd028ab33370ed53ce32e6c3cbd14ad139c
Maxym Kalysh <503292@ukr.net>
2018-12-06 10:40:06 +0200

    reflection & anotation

--- TestReflection/MyClass.java
+++ TestReflection/MyClass.java
@@ -1,11 +1,19 @@
 package TestReflection;
 
 public class MyClass {
-
-
-	@MyAnnotation(paramOne=5,paramTwo=10)
-	public void printConsole(int a, int b) {
-		System.out.println(a + b);
-
+	public int sum(int a, int b) {
+		return (a + b);
 	}
+
+	@MyAnnotation(paramOne=10,paramTwo=10)
+	public void printConsole(int a, int b) {
+		System.out.println(sum(a,b));
+
+		
+	}
+	@MyAnnotation
+	public void printConsole2(int a, int b) {
+		System.out.println(sum(a,b));
+	}
+
 }
